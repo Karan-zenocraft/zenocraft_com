9df29dca99e2a9c69669178bd4bb861c735b3ec0
root <root@localhost.creatusello.com>
2019-12-11 19:09:57 +0530

    portfolio dynamic and admin panel

--- application/models/Portfolio_Model.php
+++ application/models/Portfolio_Model.php
@@ -3,6 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Portfolio_Model extends CI_Model
 {
+    protected $table = "portfolio";
     public function getPortfolioDetails()
     {
         $query = $this->db->select('title,category,description,image,id,skill')
@@ -47,5 +48,61 @@ class Portfolio_Model extends CI_Model
                 ->get('portfolio');
         }
         return $ret->result();
+    }
+    public function get_count($category)
+    {
+        if ($category == 1) {
+            return $this->db->count_all_results($this->table);
+        } else {
+            return $this->db->where('category', $category)->count_all_results($this->table);
+        }
+    }
+
+    public function getPortfolios($limit, $start, $category)
+    {
+        if ($category == 1) {
+            $this->db->limit($limit, $start);
+            $query = $this->db->get($this->table);
+        } else {
+            $this->db->limit($limit, $start);
+            $query = $this->db->get_where($this->table, ['category' => $category]);
+        }
+        return $query->result();
+    }
+    public function getRows($params = array())
+    {
+        $this->db->select('*');
+        $this->db->from($this->table);
+
+        if (array_key_exists("where", $params)) {
+            foreach ($params['where'] as $key => $val) {
+                $this->db->where($key, $val);
+            }
+        }
+
+        if (array_key_exists("returnType", $params) && $params['returnType'] == 'count') {
+            $result = $this->db->count_all_results();
+        } else {
+            if (array_key_exists("id", $params) || (array_key_exists("returnType", $params) && $params['returnType'] == 'single')) {
+                if (!empty($params['id'])) {
+                    $this->db->where('id', $params['id']);
+                }
+                $query = $this->db->get();
+                $result = $query->row_array();
+            } else {
+                $this->db->order_by('id', 'ASC');
+                if (array_key_exists("start", $params) && array_key_exists("limit", $params)) {
+                    $this->db->limit($params['limit'], $params['start']);
+                } elseif (!array_key_exists("start", $params) && array_key_exists("limit", $params)) {
+                    $this->db->limit($params['limit']);
+                }
+
+                $query = $this->db->get();
+                $result = ($query->num_rows() > 0) ? $query->result_array() : false;
+            }
+        }
+
+        // Return fetched data
+        return $result;
     }
 }
